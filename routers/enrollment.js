a34a9414581b8fad135637a9e26be669126baf06
Banloi.com <clear1900@protonmail.com>
2020-04-08 00:46:23 +0800

    optimize

--- routers/enrollment.js
+++ routers/enrollment.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const enrollmentCtroller = require('../controllers/enrollment')
+const check = require('../controllers/check')
 
 router.get('/', enrollmentCtroller.findEnrollment)
-router.post('/', enrollmentCtroller.createEnrollment)
+router.post('/', check.checkLogin, enrollmentCtroller.createEnrollment)
 router.put('/')
-router.delete('/', enrollmentCtroller.deleteEnrollment)
+router.post('/cancel', /*  check.checkLogin, */ enrollmentCtroller.deleteEnrollment)
 
 module.exports = router
