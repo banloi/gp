1bd60b8c6257e91521987d57aab0d1df579950ba
Banloi.com <clear1900@protonmail.com>
2020-04-23 17:31:35 +0800

    添加未打分以及设置完成的路由

--- routers/activities.js
+++ routers/activities.js
@@ -7,10 +7,11 @@ router.get('/', check.checkLogin, actCtrller.actQueryGet)
 // POST 请求通常通过表单发送，并返回修改结果，常用于发布消息，新增用户，追加扩展数据库（增）
 router.post('/', check.checkAdmLogin, actCtrller.actCreatePost)
 // PUT 请求方法使用请求中的负载创建替换目标资源，幂等，一次或多次连续调用是等价的，无副作用（改）
-router.put('/', (req, res) => {})
+router.put('/complete', check.checkAdmLogin, actCtrller.complete)
 router.delete('/', actCtrller.actDelete)
 
 router.get('/enrolling', check.checkAdmLogin, actCtrller.getEnroAct)
 router.get('/underway', check.checkAdmLogin, actCtrller.getUnderway)
+router.get('/unrated', check.checkAdmLogin, actCtrller.getUnrated)
 
 module.exports = router
