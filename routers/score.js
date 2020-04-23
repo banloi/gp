d16f4e55dc50b9cd3ebd9496f020c393ce4c98b8
Banloi.com <clear1900@protonmail.com>
2020-04-23 17:33:45 +0800

    添加获取打分表和打分路由

--- routers/score.js
+++ routers/score.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const scoreController = require('../controllers/score')
+const check = require('../controllers/check')
 
 router.get('/', scoreController.getScore) // 获取分数
+router.get('/ratelist', check.checkAdmLogin, scoreController.getRateList) // 获取打分列表
 router.put('/')
-router.post('/', scoreController.createScore) // 创建分数
+router.post('/', check.checkAdmLogin, scoreController.createScore) // 创建分数
 router.delete('/')
 
 module.exports = router
