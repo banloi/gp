70a7b5dd26814327ec5738e89c6c5811f598e3d5
Banloi.com <clear1900@protonmail.com>
2020-05-18 20:50:57 +0800

    JWT 和 CORS 设置

--- config/default.js
+++ config/default.js
@@ -1,9 +1,12 @@
 module.exports = {
   port: 3000,
-  session: {
+  jwt: {
     secret: 'gp',
     key: 'graduation',
     maxAge: 2592000000
   },
-  mongoDB: 'mongodb://localhost:27017/gp'
+  mongoDB: 'mongodb://localhost:27017/gp',
+  url: {
+    cors: 'http://localhost:3001, http://192.168.50.188:10086'
+  }
 }
