963d96d713775c067e8475fbdf0c298b4270e1b4
Banloi.com <clear1900@protonmail.com>
2020-05-25 20:45:52 +0800

    修改管理员密码

--- routers/adm.js
+++ routers/adm.js
@@ -1,8 +1,9 @@
 const admController = require('../controllers/adm')
 const express = require('express')
 const router = express.Router()
+const check = require('../controllers/check')
 
 router.post('/', admController.admRegister)
 router.post('/login', admController.admLogin)
-
+router.put('/password', check.checkAdmLogin, admController.putPassword)
 module.exports = router
