055603419de8c0ad0422165b6f118a84a4d363d4
Banloi.com <clear1900@protonmail.com>
2020-05-25 20:41:17 +0800

    添加表格导入学生

--- routers/students.js
+++ routers/students.js
@@ -1,9 +1,14 @@
 const StudentControllers = require('../controllers/student')
 const experss = require('express')
 const router = experss.Router()
+const multer = require('multer')
+var storage = multer.memoryStorage()
+var upload = multer({ storage: storage })
 
 router.post('/', StudentControllers.createStudent)
 router.get('/', StudentControllers.findStudent)
-router.patch('/', StudentControllers.modifyStudent)
+router.put('/', StudentControllers.modifyStudent)
+router.post('/excel', upload.any(), StudentControllers.getFile)
+router.delete('/', StudentControllers.deleteStudent)
 
 module.exports = router
